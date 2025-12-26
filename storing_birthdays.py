a255a0e7efa997164271f379c046fc65f39ea565
seyon311 <seyonsaithridevrajaguru@gmail.com>
2025-12-26 17:39:08 +0000

    Add storing_birthdays.py to print birthday dates

---
+++ storing_birthdays.py
@@ -0,0 +1,11 @@
+adam = "31/5/2015"
+alexander = "29/2/2014"
+charlie = "7/3/2017"
+zayne = "4/1/2012"
+barry = "20/2/2002"
+
+print(adam)
+print(alexander)
+print(charlie)
+print(zayne)
+print(barry)
