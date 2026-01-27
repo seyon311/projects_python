ff149305fdc990f1910dc6469cc0bca48ca67d3b
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-01-27 16:18:25 +0000

    Add files via upload

---
+++ operator_not=to.py
@@ -0,0 +1,23 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print("A and B are different strings")
+else:
+    print("A and B are the same strings")
+
+a = 1
+b = 5
+
+if (a == 4) != (b == 5):
+    print("hello")
+
+if  a % 2 != 0:
+    print("a is an not even number")
