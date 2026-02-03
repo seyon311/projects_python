3c75001240d4731a42620bba19cc63b6b9a77977
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-02-03 16:40:18 +0000

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,13 @@
+a = 10
+b = -10
+print(f"a = {a} and b = {b}\n")
+
+print("a >> 1 =", a >> 1)
+print("a << 1 =", a << 1)
+
+a = 5
+b = -10
+print(f"a = {a} and b = {b}\n")
+
+print("a << 1 =", a << 1)
+print("b >> 1 =", b >> 1)
