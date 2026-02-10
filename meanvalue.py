b27f552d7277d086603ae4051d6a14978fc91d17
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-02-10 16:30:27 +0000

    Add files via upload

---
+++ meanvalue.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+
+sum = mean1 * total_number  
+print("The sum of 40 numbers: ", sum)
+
+num2 = sum - (wrong_number - correct_number)
+print("Sum - (wrong number - correct number) is ", num2)
+
+mean2 = num2 / total_number 
+print(mean2)
