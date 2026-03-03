0de2e73bff15564ca0e2868c01d1b3ad89b0d53c
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-03-03 15:45:10 +0000

    Add files via upload

---
+++ power.py
@@ -0,0 +1,6 @@
+base = int(input("What is the base? (Enter a number): "))
+exponent = int(input("What is the exponent? (Enter a number): "))
+
+result = base ** exponent
+
+print(f"{base} raised to the power of {exponent} is: {result}")
