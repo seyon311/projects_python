9c43876ff01028867c630c1924f9fd78f52950df
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-03-03 16:20:34 +0000

    Add files via upload

---
+++ infinity.py
@@ -0,0 +1,6 @@
+print("This program will run indefinitely. Run with caution!")
+
+input("Press Enter to start the infinite loop...")
+
+while 1>0:
+    print("This is an infinite loop, and it will keep running until you stop it.")
