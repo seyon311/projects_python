21e0101daddb0b6d7c047bc82d34070084a9d606
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-03-20 18:07:58 +0000

    Add files via upload

---
+++ def_function.py
@@ -0,0 +1,7 @@
+def well_wishes(name, occasion):
+    print(f"Dear {name}, I wish you all the best on your {occasion}!")
+
+input_name = input("Enter your name: ")
+input_occasion = input("Enter the occasion: ")
+
+well_wishes(input_name, input_occasion)
