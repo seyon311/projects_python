08b55ce500dec2d0eac3613f58ee4f7479c0d11b
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-04-09 17:09:28 +0100

    Add files via upload

---
+++ dt.py
@@ -0,0 +1,8 @@
+from datetime import date, time, datetime
+
+today = date.today()
+now = datetime.now()
+print("Today's date:", today)
+print("Current date and time:", now)
+
+print(f"Date:{today.day}/{today.month}/{today.year}")
