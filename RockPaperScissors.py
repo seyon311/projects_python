f37507b084d2f338338e900be422257935b1a7a0
seyon311 <seyonsaithridevrajaguru@gmail.com>
2026-08-22 13:39:42 +0100

    Add files via upload

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,15 +1,50 @@
 import random
 
-userchoice = input("Do you choose rock, paper or scissors? [ R/P/S ] ").upper().strip()
-Aichoice = random.choice(['R', 'P', 'S'])
+print("Lets play Rock Paper Scissors!\n")
 
-if userchoice == Aichoice:
-    print("It's a tie! Both you and the AI chose", userchoice)
-elif userchoice == 'R' and Aichoice == 'S' or userchoice == 'P' and Aichoice == 'R' or userchoice == 'S' and Aichoice == 'P':
-    print("You win! You chose", userchoice, "and the AI chose", Aichoice)
-elif userchoice == 'R' and Aichoice == 'P' or userchoice == 'P' and Aichoice == 'S' or userchoice == 'S' and Aichoice == 'R':
-    print("You lose! You chose", userchoice, "and the AI chose", Aichoice)
-else:
-    print("Invalid input. Please choose R, P, or S.")
-    
-            
+def wincond(p1, p2):
+    if p1 == p2:
+        return "draw"
+    elif p1 == "rock" and p2 == "paper":
+        return "p2"
+    elif p1 == "rock" and p2 == "scissors":
+        return "p1"
+    elif p1 == "paper" and p2 == "rock":
+        return "p1"
+    elif p1 == "paper" and p2 == "scissors":
+        return "p2"
+    elif p1 == "scissors" and p2 == "rock":
+        return "p2"
+    elif p1 == "scissors" and p2 == "paper":
+        return "p1"
+    else:
+        "N/A"
+
+
+while True:
+    optionh = input("Type rock, paper or scissors and dont worry I will choose randomly : ").lower()
+    optiona = random.randint(1, 3)
+
+    if optiona == 1:
+        optiona = "rock"
+    elif optiona == 2:
+        optiona = "paper"
+    else:
+        optiona = "scissors"
+
+    result = wincond(optionh, optiona)
+
+    if result == "draw":
+        print("It's a draw!")
+    elif result == "p1":
+        print("You won!")
+    elif result == "p2":
+        print("You lost...")
+    else:
+        print("Sorry I could understand your option.")
+
+    again = input("Would you like to play again? (yes/no) : \n")
+
+    if again != "yes":
+        print("Bye then!")
+        break
